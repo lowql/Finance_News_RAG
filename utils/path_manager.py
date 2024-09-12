ef739c772779cb7e424233fb925c194c43f69e72
vemi <kolagh@gmail.com>
2024-09-12 17:31:13 +0800

    new templates path

--- utils/path_manager.py
+++ utils/path_manager.py
@@ -1,14 +1,12 @@
 # utils/path_manager.py
 from pathlib import Path
 
-def get_data_dir():
-   return f"./dataset/news"
+""" DataSets """
+def get_dataset_dir():
+   return f"./dataset"
 
-def get_history_news_file(code):
-   """從FinMind API 爬取下來的原始資料 (url被google額外編碼)
-   date,stock_id,link,source,title
-   """
-   return f"{get_data_dir()}/{code}_history_news.csv"
+def get_download_news_file(code):
+   return f"{get_dataset_dir()}/download/{code}_history_news.csv"
  
 def get_filter_news_file(code):
    """ 
@@ -16,15 +14,26 @@ def get_filter_news_file(code):
       1. 除了YAHOO以外的消息源
       2. 無法正確解碼的URL
    """
-   return f"{get_data_dir()}/{code}_news.csv"
+   return f"{get_dataset_dir()}/filter/{code}_yahoo_news.csv"
 
 def get_news_content_file(code):
    """ 具有內文的新聞資料集
    1. metadata: date,stock_id,author,headline
    2. content: content
    """
-   return f"{get_data_dir()}/{code}_news_content.csv"
+   return f"{get_dataset_dir()}/news/{code}_news.csv"
+
+def get_company_relations():
+   return f"{get_dataset_dir()}/base/company_relations.csv"
+
+""" Templates """
+def get_templates_dir():
+   return f"./templates"
+
+def get_llama_index_template(type):
+   return f"{get_templates_dir()}/llama-index/{type}_template.jinja"
+   
 
 if __name__ == '__main__':
-    print(get_history_news_file(6125))
+    print(get_news_content_file(6125))
 

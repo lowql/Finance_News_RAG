014e2f823d1c5c48f2701f35c09f6c49e07618df
vemi <kolagh@gmail.com>
2024-10-02 19:21:07 +0800

    重要更新:: :: 打造數據集工作流 :: 打造屬性圖建置工作流

--- tests/dataset_workflow/test_fetch_news.py
+++ tests/dataset_workflow/test_fetch_news.py
@@ -1,9 +1,40 @@
 
 def test_fetch_from_FinMind():
-    pass
+    from dataset.timer import schedule_script,display_countdown,fetch_FinMind
+    import schedule
+    interval = 60  # 設置間隔時間（分鐘）
+    schedule_script(interval)
+    while fetch_FinMind() == False:
+        print("API 使用額度已達上限休息1HR")
+        display_countdown(interval)
+        schedule.run_pending()
 
+def test_record():
+    from dataset.download.helper import update_record
+    update_record()
+    
 def test_news_filter():
-    pass
+    from dataset.download.helper import read_record
+    import pandas as pd
+    from utils.path_manager import get_download_news_file,get_filter_news_file
+    from dataset.news_filter import news_filter
+    codes = read_record()
+    
+    for code in codes:
+        csv_file = get_download_news_file(code)
+        print(f"filter process {csv_file}")
+        try:
+            df = pd.read_csv(csv_file,index_col=0)
+        except:
+            print("Can't read csv_file")
+            continue
+        df = news_filter(df)
+        output_file = get_filter_news_file(code)
+        df.to_csv(output_file, index=False)
+        print(f"處理完成，結果已保存到 {output_file}")
+        print()
 
 def test_fetch_yahoo_content():
+    from dataset.news_crawler.fetch_yahoo import fetch_from_filter
+    fetch_from_filter()
     pass
--- tests/property_graph/test_build.py
+++ tests/property_graph/test_build.py
@@ -3,13 +3,15 @@ import os
 news = os.listdir('./dataset/news/')
 codes = [new.split('_')[0] for new in news]
 
+from dataset.download.helper import read_record
+codes = read_record()
 
 builder = ManualBuildPropertyGraph()
 def test_build_news():
-    builder.news_mention_company(6125)
-
+    [builder.news_mention_company(code) for code in codes]
+    
 def test_build_company_rel():
-    builder.company_rel_company(6125)
+    [builder.company_rel_company(code) for code in codes]
 
 def test_remove_all():
     builder.remove_all()

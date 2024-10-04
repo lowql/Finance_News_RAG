23f981a40dfea0b641e2f2814b7aa5e7f502dcf4
vemi <kolagh@gmail.com>
2024-10-04 20:00:07 +0800

    輸出新聞文件

--- pipeline/news.py
+++ pipeline/news.py
@@ -13,11 +13,30 @@ class Pipeline:
         from llama_index.core.schema import TextNode
         nodes = []
         for idx, document in enumerate(self.documents):
-            node = TextNode(text=document,metadata={
+            node = TextNode(
+                text=document,
+                metadata={
                 'headline':self.meta_headline[idx],
                 'author':self.meta_author[idx],
-                'time':self.meta_time[idx]})
+                'time':self.meta_time[idx]
+                })
             nodes.append(node)
         # print(nodes[0].metadata)
         # print(nodes[0].get_content())
         return nodes
+    def news_documents(self):
+        from llama_index.core.schema import Document
+        documents = []
+        for idx, content in enumerate(self.documents):
+            documents.append(Document(
+                    text=content,
+                    metadata={
+                        'headline':self.meta_headline[idx],
+                        'author':self.meta_author[idx],
+                        'time':self.meta_time[idx]
+                    },
+                    metadata_seperator="\n",
+                    metadata_template="{key} : {value} ",
+                    text_template="新聞的基本資訊:\n {metadata_str}\n\n 新聞內文:\n {content}",
+                ))
+        return documents
